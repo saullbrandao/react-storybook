cff0b38ce8cd7f9f774cef3fa5c20ad185197622
Saull Brandão <saull@outlook.com>
2021-07-22 09:36:26 -0300

    story within story

---
+++ src/components/Subscription/index.stories.jsx
@@ -0,0 +1,15 @@
+import { Primary } from '../Button/index.stories'
+import { Large } from '../Input/index.stories'
+
+export default {
+  title: 'form/Subscription',
+}
+
+export const PrimarySubscription = () => {
+  return (
+    <>
+      <Large />
+      <Primary />
+    </>
+  )
+}
